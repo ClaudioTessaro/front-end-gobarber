6496576f17725c42f7df26e1a2e4738c073cc4ec
Claudio Tessaro <claudio0190@hotmail.com>
2020-05-18 22:03:21 -0300

    Criação da base da aplicação

---
+++ src/pages/_layouts/auth/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Wrapper } from './styles';
+
+export default function AuthLayout({ children }) {
+  return <Wrapper>{children}</Wrapper>;
+}
+
+AuthLayout.prototype = {
+  children: PropTypes.element.isRequired,
+};
---
+++ src/pages/_layouts/default/index.js
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Wrapper } from './styles';
+
+export default function DefaultLayout({ children }) {
+  return <Wrapper>{children}</Wrapper>;
+}
+
+DefaultLayout.prototype = {
+  children: PropTypes.element.isRequired,
+};
