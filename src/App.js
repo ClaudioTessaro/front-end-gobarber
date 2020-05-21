f2b0cb64c156bfe9d9e01004660288f3011b94c1
Claudio Tessaro <claudio0190@hotmail.com>
2020-05-21 00:15:23 -0300

    Autenticação concluida

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
-import './config/ReactotronConfig';
 import React from 'react';
+import { ToastContainer } from 'react-toastify';
+import { PersistGate } from 'redux-persist/integration/react';
 import { Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
+
+import './config/ReactotronConfig';
+
 import Routes from './routes';
 import history from './services/history';
 import GlobalStyle from './styles/global';
-import store from './store';
+import { store, persistor } from './store';
 
 function App() {
   return (
     <Provider store={store}>
-      <Router history={history}>
-        <Routes />
-        <GlobalStyle />
-      </Router>
+      <PersistGate persistor={persistor}>
+        <Router history={history}>
+          <Routes />
+          <GlobalStyle />
+          <ToastContainer autoColse={3000} />
+        </Router>
+      </PersistGate>
     </Provider>
   );
 }
