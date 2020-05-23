88ebe5f18c9f0e8c50ac14acebe9ba22dfc39a92
Claudio Tessaro <claudio0190@hotmail.com>
2020-05-22 21:58:37 -0300

    Inserindo as notificações

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '~/assets/logo-purple.svg';
 
+import Notifications from '~/components/Notifications';
+
 import { Container, Content, Profile } from './styles';
 
 export default function Header() {
@@ -14,6 +16,7 @@ export default function Header() {
         </nav>
 
         <aside>
+          <Notifications />
           <Profile>
             <div>
               <strong>Claudio Tesaro</strong>
